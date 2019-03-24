d1d338de3a1015cbdc08761c79eaafdc6cadf267
JoeT <jtjaard@live.com>
2019-03-23 23:34:25 -0700

    Added Automock attribute that i can use with Xunit for easy mocking of classes.

---
+++ Ataraxia.Tools.Testing/AutoMockAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using AutoFixture;
+using AutoFixture.AutoMoq;
+using AutoFixture.Xunit2;
+
+namespace Ataraxia.Tools.Testing
+{
+    public class AutoMockAttribute : AutoDataAttribute
+    {
+        public AutoMockAttribute() : base(() => new Fixture().Customize(new AutoMoqCustomization())) { }
+    }
+}
